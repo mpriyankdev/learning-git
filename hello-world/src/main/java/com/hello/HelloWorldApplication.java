58b5960e743c9a2fea6880cdd59018290077f878
mpriyankdev <mpriyank.dev@gmail.com>
2018-10-24 23:20:22 +0530

    commit using STS

--- hello-world/src/main/java/com/hello/HelloWorldApplication.java
+++ hello-world/src/main/java/com/hello/HelloWorldApplication.java
@@ -1,7 +1,16 @@
 package com.hello;
 
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -10,3 +19,18 @@ public class HelloWorldApplication {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 }
+
+@RestController
+class HelloWorldController{
+	
+	
+	@Value("${classpath:name}")
+	private String name;
+	
+	@GetMapping("/hello")
+	public String method1() {
+		return "Hello World"+name;
+	}
+	
+}
+
